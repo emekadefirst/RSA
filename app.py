9447eb92a0501f10dc925d1d918788b776abeb46
President Carter <olabodebalogun80@gmail.com>
2024-03-20 06:21:17 +0100

    commit

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 from main import Scheme
 
-public_key = int(input("Enter Your Public-key\n>> "))
-secret_key = int(input("Enter Your Secret-key\n>> "))
-message = str(input("Enter Your message\n>> ")).strip()
-
 def alphabet_to_number(alphabet):
     return ord(alphabet.lower()) - 96 if alphabet.isalpha() else None
 
-# Convert message to numbers
-data = [alphabet_to_number(char) for char in message if alphabet_to_number(char) is not None]
+def number_to_alphabet(number):
+    if number == 0:  # Representing space
+        return ' '  # Return space
+    elif 1 <= number <= 26:
+        return chr(number + 96)  # For letters
+    else:
+        return "Not a valid number."
 
-# Encrypting the message
-message_obj = Scheme(secret_key, public_key, data)
-encrypted_data = message_obj.encrypt()
-encrypted_string = ''.join(str(digit) for digit in encrypted_data)
+response = str(input("What would you like to do\n1. Encrypt data\n2. Decrypt data\n>> "))
+public_key = int(input("Enter Your Public-key\n>> "))
+secret_key = int(input("Enter Your Secret-key\n>> "))
 
-print("Encrypted data:", encrypted_string)
-
-# # Decrypting the encrypted message
-# decrypted = message_obj.decrypt(encrypted_data)
-# decrypted_string = ''.join(str(digit) for digit in decrypted)
-
-# print("Decrypted data:", decrypted_string)
+if response == "1":
+    message = str(input("Enter Your message\n>> ")).strip()
+    data = [alphabet_to_number(char) for char in message if alphabet_to_number(char) is not None]
+    message_obj = Scheme(secret_key, public_key, data)
+    encrypted_data = message_obj.encrypt()
+    encrypted_string = ''.join(str(digit) for digit in encrypted_data)
+    print("Encrypted data:", encrypted_string)
+elif response == "2":
+    message = str(input("Enter Your message\n>> ")).strip()
+    data = [int(digit) for digit in message if digit.isdigit()]
+    message_obj = Scheme(secret_key, public_key, data)
+    
+    # Decrypting the encrypted message
+    decrypted_data = message_obj.decrypt(data)
+    decrypted_string = ''.join(number_to_alphabet(digit) if digit is not None else '' for digit in decrypted_data)
+    print("Decrypted data:", decrypted_string)
+else:
+    print("Invalid option. Please select either '1' or '2'.")
